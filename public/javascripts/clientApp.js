f355384437d91dcf6d258df3b6a01367213daba4
babytruckdriver <babytruckdriver@gmail.com>
2014-02-16 20:22:23 +0100

    Added element caching

--- public/javascripts/clientApp.js
+++ public/javascripts/clientApp.js
@@ -9,7 +9,7 @@ jQuery(function($) {
 
         var ENTER_KEY = 13;
 
-        //TODO Objeto contenedor de utilidades. Aquí podría ir el esqueleto de la llamada vía Ajax
+        //FUTURE Objeto contenedor de utilidades. Aquí podría ir el esqueleto de la llamada vía Ajax
         //Estas funciones, si su uso es lo suficientemente común, podrían ir en un archivo '.js' a parte.
         /*var utils = {
 
@@ -23,15 +23,29 @@ jQuery(function($) {
                         this.bindEvents();
                 },
                 cacheElements: function () {
-
+                        this.weatherNode = $("#weatherNode");
+                        this.condicionesActuales = this.weatherNode.find("#condicionesActuales");
+                        this.temperatura = this.condicionesActuales.find(".temperatura");
+                        this.estado = this.condicionesActuales.find(".estado");
+                        this.localidadTemperatura = this.condicionesActuales.find(".localidad-temperatura");
+                        this.imagenActual = this.condicionesActuales.find(".imagen-actual");
+                        this.cuerpo = this.weatherNode.find(".cuerpo");
+                        this.localidad = this.cuerpo.find("#localidad");
+                        this.indicadorAjaxEnCurso = this.cuerpo.find("#indicadorAjaxEnCurso");
+                        this.btoGetWeatherInfo = this.cuerpo.find("#getWeatherInfo");
+                        this.forecastContainer = this.cuerpo.find("#forecast-container");
+                        this.cabeceras = this.forecastContainer.find(".cabecera");
+                        this.validacionesContainer = this.cuerpo.find("#validaciones-container");
+                        this.errorContainer = this.cuerpo.find("#error-container");
+                        this.txtLocalidad = this.cuerpo.find(".localidad");
                 },
                 bindEvents: function () {
                         //Al clicar el botón se consulta el tiempo para la localidad indicada
-                        $("#getWeatherInfo").on("click", this.eventWeatherInfo.bind(this));
-                        $("#localidad").on("keyup", this.eventLocalidad.bind(this));
+                        this.btoGetWeatherInfo.on("click", this.eventWeatherInfo.bind(this));
+                        this.localidad.on("keyup", this.eventLocalidad.bind(this));
                         //Al clicar en el campo de entrada quitar la alerta visual de error por campo vacío
-                        $("#localidad").on("click", function () {
-                            $("#localidad").removeClass("error-input");
+                        this.localidad.on("click", function (event) {
+                            $(event.target).removeClass("error-input");
                         });
                 },
 
@@ -40,39 +54,39 @@ jQuery(function($) {
                 eventWeatherInfo: function () {
                         //Validaciones sobre los campos de entrada
                         var erroresEntrada = false;
-                        if(!$("#localidad").val()) {
+                        if(!this.localidad.val()) {
                                 this.muestraValidaciones(["Es necesario especificar una localidad"]);
-                                $("#localidad").focus();
-                                $("#localidad").addClass("error-input");
+                                this.localidad.focus().addClass("error-input");
                                 erroresEntrada = true;
                         }
 
                         if(!erroresEntrada) {
-                                $("#validaciones-container").hide();
+                                this.validacionesContainer.hide();
                                 this.getWeatherInfo();
                         }
                 },
                 eventLocalidad: function (event) {
                         //Al comenzar a teclear en el campo de entrada quitar la alerta visual de error por campo vacío
-                        $("#localidad").removeClass("error-input");
+                        this.localidad.removeClass("error-input");
 
                         //Si la tecla pulsada es un 'Intro' lanzar el evento 'click' del botón
                         if(event.keyCode === ENTER_KEY) {
-                                $("#getWeatherInfo").click();
+                                this.btoGetWeatherInfo.click();
                                 return true;
                         }
 
-                        var txtLocalidad = $(".localidad");
-                        txtLocalidad.text($(this).val());
+                        //Se utiliza 'event.target' en vez de 'this', ya que este en vez de hacer referencia al objeto
+                        //que ha lanzado el evento hace referenci al objeto App porque se ha invocado así: '.bind(this)'?
+                        this.txtLocalidad.text($(event.target).val());
 
                         //Si el campo está vacío
-                        if(!$(this).val()) {
-                                txtLocalidad.text("...");
+                        if(!$(event.target).val()) {
+                                this.txtLocalidad.text("...");
                         }
 
                         //Si se han introducido más de 3 caracteres se procede a buscar localidades
                         //que coincidan con esos caracteres
-                        //TODO Descomentar cuando se sepa qué hacer con la lista de localidades devueltas
+                        //FUTURE Descomentar cuando se sepa qué hacer con la lista de localidades devueltas
                         /*if($(this).val().length >= 4) {
                         getLocalidades(this);
                         }*/
@@ -80,16 +94,15 @@ jQuery(function($) {
                 //Muestra mensajes de validación
                 muestraValidaciones: function (arrValidaciones) {
                         //La información meteorológica anteriormente cargada se esconde
-                        $("#forecast-container").hide();
-                        $("#condicionesActuales").hide();
+                        this.forecastContainer.hide();
+                        this.condicionesActuales.hide();
 
-                        var validacionesContainer = $("#validaciones-container");
-                        validacionesContainer.html("<span class='centrado'>" + arrValidaciones[0] + "<span>");
+                        this.validacionesContainer.html("<span class='centrado'>" + arrValidaciones[0] + "<span>");
 
-                        if(validacionesContainer.is(":visible")) {
-                                validacionesContainer.slideUp(200).delay(200).slideDown(200);
+                        if(this.validacionesContainer.is(":visible")) {
+                                this.validacionesContainer.slideUp(200).delay(200).slideDown(200);
                         } else {
-                                validacionesContainer.slideDown(200);
+                                this.validacionesContainer.slideDown(200);
                         }
                 },
 
@@ -97,16 +110,15 @@ jQuery(function($) {
                 //err = { cod, desc }
                 errorHandle: function (err) {
                         //La información meteorológica anteriormente cargada se oculta, además de los errores que se estén ya mostrando
-                        $("#forecast-container").hide();
-                        $("#condicionesActuales").hide();
-                        var errorContainer = $("#error-container");
-                        errorContainer.hide();
+                        this.forecastContainer.hide();
+                        this.condicionesActuales.hide();
+                        this.errorContainer.hide();
 
                         var errorMsg = err.statusText +
                         "\n Error " + err.status;
 
-                        errorContainer.html(errorMsg);
-                        errorContainer.slideDown(200).delay(3000).slideUp(2000);
+                        this.errorContainer.html(errorMsg);
+                        this.errorContainer.slideDown(200).delay(3000).slideUp(2000);
                 },
                 //Recupera la información meteorológica para la localización introducida
                 getWeatherInfo: function () {
@@ -118,12 +130,16 @@ jQuery(function($) {
                                 //Objecto con los datos de entrada de la petición
                                 var datos = {
                                     formato: "json",
-                                    localidad: $("#localidad").val(),
+                                    localidad: this.localidad.val(),
                                     numDias: 4
                                 };
 
                                 ajaxInProgress = true;
-                                $("#indicadorAjaxEnCurso").show();
+                                this.indicadorAjaxEnCurso.show();
+
+                                //Se quiere utilizar 'this' dentro de una función [complete] de un objeto, pero la función tendrá como 'this' el objeto en el que fué creada
+                                //por lo que guardo el 'this' actual en una variable alcanzable desde dentro de la función llamada 'that'
+                                var that = this;
 
                                 //Configuración y llamada al servicio RESTful vía Ajax
                                 $.ajax({
@@ -139,7 +155,7 @@ jQuery(function($) {
                                         //TODO He comprobado que si se produce una excepción en la función 'success' la función 'complete' no se ejecuta. Investigar.
                                         complete: function () {
                                                 ajaxInProgress = false;
-                                                $("#indicadorAjaxEnCurso").hide();
+                                                that.indicadorAjaxEnCurso.hide();
                                         }
                                 });
                         }// End if
@@ -152,27 +168,28 @@ jQuery(function($) {
                                 //Si la respuesta no contiene errores
                                 if(json.info.data.error === undefined) {
                                         //Carga del tiempo actual
-                                        var temperatura = $(".temperatura").text(json.info.data.current_condition[0].temp_C + "Cº");
-                                        $(".estado").text(json.info.data.current_condition[0].weatherDesc[0].value);
+                                        var temperatura = this.temperatura.text(json.info.data.current_condition[0].temp_C + "Cº");
+                                        this.estado.text(json.info.data.current_condition[0].weatherDesc[0].value);
                                         $(temperatura[0]).closest("div").show();
-                                        $(".localidad-temperatura").text(json.info.data.request[0].query);
+                                        this.localidadTemperatura.text(json.info.data.request[0].query);
 
-                                        //TODO utilizar los siguientes datos referentes al tiempo actual
+                                        //FUTURE utilizar los siguientes datos referentes al tiempo actual
                                         /*$("#cloud-cover").text(json.info.data.current_condition[0].cloudcover + "%");
                                         $("#precipitacion").text(json.info.data.current_condition[0].precipMM + "mm");
                                         $("#velocidad-viento").text(json.info.data.current_condition[0].windspeedKmph + "Km/h");*/
 
                                         //Carga de la imagen asociada al tiempo actual
-                                        var imagenActual = $(".imagen-actual");
-                                        imagenActual.attr("src", json.info.data.current_condition[0].weatherIconUrl[0].value);
+                                        this.imagenActual.attr("src", json.info.data.current_condition[0].weatherIconUrl[0].value);
 
 
 
                                         //Presentación de la previsión meteorológica (diferentes de temperatura actual)
                                         var jsonForecast = json.info.data.weather;
-                                        var cabeceras = $(".cabecera");
 
                                         if(jsonForecast !== undefined) {
+                                                //Se quiere utilizar 'this' dentro del bucle, pero los bucles crean sus propios 'this'
+                                                //por lo que guardo el 'this' actual en una variable alcanzable desde dentro del bucle llamada 'that'
+                                                var that = this;
                                                 $.each(jsonForecast, function (key, value) {
 
                                                         //Mostrar fecha en formato España
@@ -188,7 +205,7 @@ jQuery(function($) {
                                                                 fechaFormateada = "Hoy";
                                                         }
 
-                                                        $(cabeceras[key]).text(fechaFormateada);
+                                                        $(that.cabeceras[key]).text(fechaFormateada);
                                                         $("#temperatura" + key).text(value.tempMaxC + "/" + value.tempMinC + "Cº Max/min");
                                                         $("#estado" + key).text(value.weatherDesc[0].value);
                                                         $("#precipitacion" + key).text(value.precipMM + "mm");
@@ -200,14 +217,12 @@ jQuery(function($) {
                                         }
 
                                         //Animación para la presentación del listado de datos y la imagen
-                                        var forecastContainer = $("#forecast-container");
-                                        var condicionesActuales = $("#condicionesActuales");
-                                        if(forecastContainer.is(":visible")) {
-                                                forecastContainer.slideUp(200).delay(200).slideDown(1000);
-                                                condicionesActuales.hide().slideDown("1000");
+                                        if(this.forecastContainer.is(":visible")) {
+                                                this.forecastContainer.slideUp(200).delay(200).slideDown(1000);
+                                                this.condicionesActuales.hide().slideDown("1000");
                                         } else {
-                                                forecastContainer.slideDown(1000);
-                                                condicionesActuales.hide().slideDown("1000");
+                                                this.forecastContainer.slideDown(1000);
+                                                this.condicionesActuales.hide().slideDown("1000");
                                         }
                                 } else {
                                         //Se pasa el error a la función manejadora de errores
@@ -219,7 +234,7 @@ jQuery(function($) {
                 }
                 //Busca las posibles localizaciones según el critério de búsqueda utilizado
                /* getLocalidades: function (localidad) {
-                    //TODO Si con una consulta no ha encontrado nada no llamar de nuevo al servicio hasta que la consulta se modifique.
+                    //FUTURE Si con una consulta no ha encontrado nada no llamar de nuevo al servicio hasta que la consulta se modifique.
                     //Ej: 'casavo' no encuentra nada. 'casavoo' no buscarlo poque no va a encontrar nada tampoco.
                     //Actualización: Estudiar el comportamiento del servicio, porque creo que no funciona como describo arriba
 
@@ -262,7 +277,7 @@ jQuery(function($) {
                     if(json.ok) {
                         //Si la respuesta no contiene errores
                         if(json.info.data == undefined) {
-                            //TODO hacer algo con los resultados obtenidos de buscar localidades según se va escribiendo
+                            //FUTURE hacer algo con los resultados obtenidos de buscar localidades según se va escribiendo
                             //json.info.search_api.result[0].areaName[0].value);
                         } else {
                             //No se encuentran localidades con los datoa introducidos.
