6f08ccf5ac4de7e3e67344a3fe12ad28465f7442
smux <babytruckdriver@gmail.com>
2014-03-03 10:59:13 +0100

    New organization of the file

--- app.js
+++ app.js
@@ -7,38 +7,33 @@
 
 "use strict";
 
-var express = require('express'),
-        //routes = require('./routes'),
-        Document,
-        http = require("http");
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+//var routes = require('./routes');
+var http = require('http');
+var path = require('path');
 
 var app = express();
 
-// Configuration
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
 
-var oneHour = 3600;
-
-app.configure(function () {
-        app.set('views', __dirname + '/views');
-        app.set('view engine', 'jade');
-        app.use(express.bodyParser());
-        app.use(express.methodOverride());
-        app.use(app.router);
-        app.use(express.static(__dirname + '/public'));
-});
-
-app.configure('development', function () {
-        app.use(express.logger());
-        app.use(express.errorHandler({
-                dumpExceptions: true,
-                showStack: true
-        }));
-});
-
-app.configure('production', function () {
-        app.use(express.logger());
+// development only
+if ('development' == app.get('env')) {
         app.use(express.errorHandler());
-});
+}
 
 
 // Métodos y objetos de uso común
